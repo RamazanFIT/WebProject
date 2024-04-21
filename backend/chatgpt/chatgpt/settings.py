dbd223d0c510de422f7db556e73f70a16a0a9e5a
syrlybajramazan <syrlybai056@gmail.com>
2024-04-21 17:14:41 +0500

    upd

--- backend/chatgpt/chatgpt/settings.py
+++ backend/chatgpt/chatgpt/settings.py
@@ -42,10 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'rest_framework',
     'drf_yasg',
     'corsheaders',
-    'users',
     'chat'
     
 ]
